9e4c54148d0482abeb2542afad316b7d663d0894
Darko <bradit.digg@gmail.com>
2016-05-05 13:43:26 +0200

    Homework

---
+++ Homework1 05.05.2016/Homework/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public string Index()
+        {
+            return "default";
+        }
+
+
+        public ActionResult Fruits()
+        {
+            var fruits = new List<string>() { "Apple", "Orange" };
+            ViewBag.Fruits = fruits;
+            return View();
+        }
+
+
+        public int Vegetables()
+        {
+            return 5;
+        }
+
+
+        public string Meat()
+        {
+            return "No meat here";
+        }
+
+    }
+}
---
+++ Homework1 05.05.2016/Homework/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public string Index()
+        {
+            return "this is the index";
+        }
+
+        public ActionResult Add()
+        {
+            var users = new List<string>() { "Hajan", "Barry", "John" };
+            ViewBag.Users = users;
+            return View();
+        }
+
+        public int Remove()
+        {
+            return 3;
+        }
+
+        public string Edit()
+        {
+            return "Editing the users yo";
+        }
+    }
+}
