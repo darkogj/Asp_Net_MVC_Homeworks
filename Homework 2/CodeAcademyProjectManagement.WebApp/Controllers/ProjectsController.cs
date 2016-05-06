c56be6a5bfc8a88c5d15b8afc56918b6914dd01a
Darko <bradit.digg@gmail.com>
2016-05-06 21:55:10 +0200

    homework 2

---
+++ Homework 2/CodeAcademyProjectManagement.WebApp/Controllers/ProjectsController.cs
@@ -0,0 +1,77 @@
+﻿using CodeAcademyProjectManagement.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace CodeAcademyProjectManagement.WebApp.Controllers
+{
+
+    public class ProjectsController : Controller
+    {
+        // GET: Projects
+        public ActionResult Index()
+        {
+            // List of all projects + favorite projects
+            // Important: keep in mind longer list of projects migt need pagination
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Create(string name, string desc, int estimate)
+        {
+            return RedirectToAction("Test", new
+            {
+                name = name,
+                desc = desc,
+                estimate = estimate
+            });
+        }
+
+        public ActionResult Test(string name, string desc, int estimate)
+        {
+            ViewBag.Name = name;
+            ViewBag.Desc = desc;
+            ViewBag.Estimate = estimate;
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Update(int id)
+        {
+            var projects = Projects.GetAll();
+            var foundProject = projects.First(x => x.ID == id);
+            ViewBag.project = foundProject;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Update(int id, string name, string desc, int estimate)
+        {
+            return RedirectToAction("Test", new
+            {
+                name = name,
+                desc = desc,
+                estimate = estimate
+            });
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        public ActionResult Favorite(int id, bool favorite)
+        {
+            return View();
+        }
+    }
+}
---
+++ Homework 2/CodeAcademyProjectManagement.WebApp/Models/Projects.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeAcademyProjectManagement.WebApp.Models
+{
+    public class Projects
+    {
+        public static List<Project> GetAll()
+        {
+            List<Project> projects = new List<Project>();
+            projects.Add(new Project
+            {
+                ID = 1,
+                Name = "A",
+                Estimate = 100,
+                Description = "Project A Description"
+            });
+
+            projects.Add(new Project
+            {
+                ID = 2,
+                Name = "B",
+                Estimate = 300,
+                Description = "Project B Description"
+            });
+
+            projects.Add(new Project
+            {
+                ID = 3,
+                Name = "C",
+                Estimate = 400,
+                Description = "Project C Description"
+            });
+
+            projects.Add(new Project
+            {
+                ID = 4,
+                Name = "D",
+                Estimate = 700,
+                Description = "Project D Description"
+            });
+
+            return projects;
+        }
+    }
+
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Estimate { get; set; }
+    }
+}
